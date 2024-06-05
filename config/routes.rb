1db8e4f42cb6033942532488d7ba2e3c5d3957f1
daisuke-shimura <168567272+daisuke-shimura@users.noreply.github.com>
2024-06-05 14:28:30 +0000

    [Finish]９章まで

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  #get 'post_images/new'
+  #get 'post_images/index'
+  #get 'post_images/show'
   devise_for :users
   root to:"homes#top"
+
+  resources :post_images, only: [:new, :index, :show]
 
   get 'homes/about' => 'homes#about', as: 'about'
 
---
+++ db/migrate/20240605134943_add_caption_from_post_images.rb
@@ -0,0 +1,4 @@
+class AddCaptionFromPostImages < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
